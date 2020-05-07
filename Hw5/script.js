ee96eac3c96620cf717e49339cd7a7becf1e0759
neruskiy <you@example.com>
2020-05-07 22:10:40 +0300

    f

--- Hw5/script.js
+++ Hw5/script.js
@@ -1,7 +1,4 @@
-"use strict"
-
 //----First-----
-
 const Account = function(login, email) {
     this.login = login;
     this.email = email;
@@ -18,10 +15,9 @@ mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 const poly = new Account('Poly', 'poly@mail.com');
 
 poly.getInfo(); // Login: Poly, Email: poly@mail.com
-console.log(Account.prototype.getInfo); // function
+console.log(Account.prototype.getInfo);
 
 //------Second----------
-
 const User = function(name, age, email) {
     this.name = name;
     this.age = age;
@@ -34,14 +30,13 @@ User.prototype.getInfo2 = function() {
 
 const mango2 = new User('Mango',2, 20);
   
-mango2.getInfo2(); // User Mango is 2 years old and has 20 followers
+mango2.getInfo2();
   
 const poly2 = new User('Poly', 3, 17);
   
-  poly2.getInfo2(); // User Poly is 3 years old and has 17 followers
+  poly2.getInfo2();
 
 //----Third---
-
 const Storage = function(items){
     this.items = items;
 }
@@ -69,16 +64,15 @@ Storage.prototype.removeItem = function(item){
 
 
   const items = storage.getItems();
-  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  console.table(items);
   
   storage.addItem('Дроид');
-  console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  console.table(storage.items); 
   
   storage.removeItem('Пролонгер');
-  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+  console.table(storage.items); 
 
 //------Fourth----
-
 const StringBuilder = function(value){
     this._value = value;
 }
@@ -89,42 +83,34 @@ StringBuilder.prototype.append = function(str){
     this._value = this._value+str;
     return this._value
 }
-
-
 StringBuilder.prototype.prepend = function(str){
     this._value = str + this._value;
     return this._value
 }
-
-
 StringBuilder.prototype.pad = function(str){
     this._value = str+this._value+str;
     return this._value
 }
-
-
 StringBuilder.prototype.value = function(){
     console.log(this._value)
 }
 
 builder._value = builder.append('^');
-console.log(builder.value()); // '.^'
+console.log(builder.value());
 
 builder.prepend('^');
-console.log(builder.value()); // '^.^'
+console.log(builder.value());
 
 builder.pad('=');
-console.log(builder.value()); // '=^.^='
+console.log(builder.value());
 
 //------Fifth---
-
 class Car {
     /*
      * Добавь статический метод `getSpecs(car)`,
      * который принимает объект-машину как параметр и выводит
      * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
      */
-  
     /*
      * Конструктор получает объект настроек.
      *
@@ -152,10 +138,6 @@ class Car {
         `)
     }
   
-    /*
-     * Добавь геттер и сеттер для свойства price,
-     * который будет работать с свойством цены автомобиля.
-     */
     get price() {
         return this._price
     }
@@ -163,49 +145,27 @@ class Car {
         this._price = newprice;
     }
   
-    /*
-     * Добавь код для того чтобы завести автомобиль
-     * Записывает в свойство isOn значение true
-     */
     turnOn() {
         this._isOn = true;
     }
-  
-    /*
-     * Добавь код для того чтобы заглушить автомобиль
-     * Записывает в свойство isOn значение false,
-     * и сбрасывает текущую скорость в 0
-     */
+
     turnOff() {
         this._isOn = false;
         this._speed = 0;
     }
   
-    /*
-     * Добавялет к свойству speed полученное значение,
-     * при условии что результирующая скорость
-     * не больше чем значение свойства maxSpeed
-     */
     accelerate(value) {
         if(value <= this._maxSpeed){
             this._speed = value;
         }
     }
-  
-    /*
-     * Отнимает от свойства speed полученное значение,
-     * при условии что результирующая скорость не меньше нуля
-     */
+
     decelerate(value) {
         if(this._speed - value > 0){
             this._speed = this._speed - value;
         }
     }
   
-    /*
-     * Добавляет в поле distance киллометраж (hours * speed),
-     * но только в том случае если машина заведена!
-     */
     drive(hours) {
         if(this._isOn){
             this._distance = hours * this._speed;
@@ -220,15 +180,13 @@ class Car {
   mustang.drive(2);
   
   mustang.getSpecs();
-  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
   
   mustang.decelerate(20);
   mustang.drive(1);
   mustang.turnOff();
   
   mustang.getSpecs();
-  // maxSpeed: 200, speed: 0, isOn: false, distance: 30, price: 2000
   
-  console.log(mustang._price); // 2000
+  console.log(mustang._price);
   mustang._price = 4000;
-  console.log(mustang._price); // 4000
+  console.log(mustang._price); 
