d0c1d76d3f8ea28635b3f768bc5e109c1d170c7b
neruskiy <you@example.com>
2020-05-13 15:45:33 +0300

    f

--- Hw7/script.js
+++ Hw7/script.js
@@ -2,7 +2,6 @@ const refs = {
     categories: document.getElementById("categories")
 };
 console.log(refs.categories.children.length);
-//console.log(refs.categories.children.length);
 const arr = Array.from(refs.categories.children[1]);
 const all = refs.categories.children.array.forEach(element => {
     console.log(elem.children[1].children.length)
